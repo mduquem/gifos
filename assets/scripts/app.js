add3b078592c22b5e6693a0835b07eae2234930f
Miguel Duque <mduquem1@gmail.com>
2020-04-14 13:35:55 -0500

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+const themeToggler = document.getElementById('theme-toggler');
+
+themeToggler.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+   console.log('theme clicked');
+}
